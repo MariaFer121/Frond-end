b1010830a57ec26b47dce2a90e898effafa11ea8
Maria fernanda <95837379+MariaFer121@users.noreply.github.com>
2022-07-21 21:54:04 -0400

    ingreso de variables

--- Test-Front-End-Javascipt/script.js
+++ Test-Front-End-Javascipt/script.js
@@ -1,4 +1,18 @@
+let pacientes = []
 let guardar = () => {
+    let name = document.getElementById("name1")
+    let last_name = document.getElementById("last_name1")
+    let last_mother_name = document.getElementById("last_mother_name1")
+    let age = document.getElementById("age1")
+    let genero = document.getElementById("inputGroupSelect01")
+    let doctor = document.getElementById("inputGroupSelect02")
 
-    
+    let error_name = document.getElementById("error_name")
+    let error_last_name = document.getElementById("error_last_name")
+    let error_last_mother_name = document.getElementById("error_last_mother_name")
+    let error_age = document.getElementById("error_age")
+    let error_genero = document.getElementById("error_genero")
+    let error_doctor = document.getElementById("error_doctor")
+    let error_pacientes = document.getElementById("error_pacientes")
+
 }
