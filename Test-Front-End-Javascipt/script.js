eff02514b51d8889ac168835619c5981c03dc77c
Maria fernanda <95837379+MariaFer121@users.noreply.github.com>
2022-07-21 21:56:17 -0400

    Funcion de validacion de fromulario

--- Test-Front-End-Javascipt/script.js
+++ Test-Front-End-Javascipt/script.js
@@ -16,3 +16,54 @@ let guardar = () => {
     let error_pacientes = document.getElementById("error_pacientes")
 
 }
+function Validacion(name, last_name, last_mother_name, age, genero, doctor, error_name, error_last_name, error_last_mother_name, error_age, error_genero, error_doctor) {
+    let errors = []
+
+    if (name.value == "") {
+        errors.push("Debe ingresar el nombre")
+        error_name.classList.remove("error")
+    } else {
+        error_name.classList.add("error")
+    }
+
+    if (last_name.value == "") {
+        errors.push("Debe ingresar el apellido")
+        error_last_name.classList.remove("error")
+    } else {
+        error_last_name.classList.add("error")
+    }
+
+    if (last_mother_name.value == "") {
+        errors.push("Debe ingresar el segundo apellido")
+        error_last_mother_name.classList.remove("error")
+    } else {
+        error_last_mother_name.classList.add("error")
+    }
+
+    if (age.value == "") {
+        errors.push("Debe ingresar la edad")
+        error_age.classList.remove("error")
+    } else {
+        error_age.classList.add("error")
+    }
+
+    if (genero.value == "0") {
+        errors.push("Debe seleccionar el genero")
+        error_genero.classList.remove("error")
+    } else {
+        error_genero.classList.add("error")
+    }
+
+    if (doctor.value == "0") {
+        errors.push("Debe seleccionar el doctor")
+        error_doctor.classList.remove("error")
+    } else {
+        error_doctor.classList.add("error")
+    }
+
+    if (errors.length > 0) {
+        return true
+    } else {
+        return false
+    }
+}
