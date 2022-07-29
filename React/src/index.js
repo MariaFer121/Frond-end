8a1ca26319b3b63d7b7b1f30a67b57ba864266de
Maria fernanda <95837379+MariaFer121@users.noreply.github.com>
2022-07-29 00:32:14 -0400

    Index

--- React/src/index.js
+++ React/src/index.js
@@ -9,7 +9,7 @@ import Test from './Test/Test'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Formulario />
+    </>
   </React.StrictMode>
 );
 
